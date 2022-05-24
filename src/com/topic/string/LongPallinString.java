ebd5642bf63ca8f181463606c1c3f99fcba39c38
Sneha Patro <snehashruti.patro@gmail.com>
2022-05-25 02:51:07 +0530

    strings

---
+++ src/com/topic/string/LongPallinString.java
@@ -0,0 +1,40 @@
+package com.topic.string;
+
+public class LongPallinString {
+	static String longPallin(String str) {
+		
+		if(str == null || str.length() <1)
+			return str;
+		int start=0;
+		int end =0;
+		for (int i=0;i<str.length();i++) {
+			int len1 = expand(str,i,i);
+			int len2 = expand(str,i,i+1);
+			int len = Math.max(len1, len2);
+			if(len>end-start+1) {
+				start= i-(len-1)/2;
+				end = i+(len/2);
+				System.out.print("start: "+start+" end: "+end);
+			}			
+		}
+		
+		return str.substring(start,end+1);
+	}
+	
+	static int expand(String str,int left, int right) {
+		if(str == null || left > right)
+			return 0;
+		while(left >=0 && right <str.length() && str.charAt(left) == str.charAt(right)) {
+			left--;
+			right++;
+		}
+		System.out.print(right-left-1+" ;");
+		return right-left-1;	
+	}
+	public static void main(String[] args) {
+		String str = "racecar";
+		System.out.println(str);
+		System.out.print(longPallin(str));
+	}
+
+}
